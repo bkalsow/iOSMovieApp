d31a9fba4c2fe626d70f0f5cb8246f21a77f0b39
Brandan Kalsow <bdkalsow@yahoo.com>
2020-08-18 08:15:36 -0500

    Initial commit, blank Xcode project

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Brandan Kalsow on 8/18/20.
+//  Copyright © 2020 Virgin Pulse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
