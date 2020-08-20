344f92f3a66297f3df2434f6f51ff583cd95b2f2
Brandan Kalsow <bdkalsow@yahoo.com>
2020-08-20 11:56:51 -0500

    Can now display the date that each movie was released

--- MovieApp/MovieApp/Controllers/MovieManager.swift
+++ MovieApp/MovieApp/Controllers/MovieManager.swift
@@ -1,3 +1,10 @@
+//
+//  MovieManager.swift
+//  MovieApp
+//
+//  Created by Brandan Kalsow on 8/19/20.
+//  Copyright © 2020 Virgin Pulse. All rights reserved.
+//
 
 import Foundation
 import CoreData
@@ -7,24 +14,34 @@ class MovieManager: UITableViewController {
 
   let defaultSession = URLSession(configuration: .default)
   
+    //Get the Core Data context
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
   var dataTask: URLSessionDataTask?
   var errorMessage = ""
+    
+    //Store the results in an array
   var moviesResult: [Movie] = []
   
+    //Base URL that images come from
   private static let BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w92"
   
   typealias JSONDictionary = [String: Any]
   typealias QueryResult = ([Movie]?, String) -> Void
   
+    /**
+     * Fetches the search results from the server
+     * @param searchTerm the term to search for
+     */
     func getSearchResults(searchTerm: String, completion: @escaping QueryResult, page: Int = 1) {
-
+        //Cancel any outstanding data tasks
     dataTask?.cancel()
-    //https://api.themoviedb.org/3/search/movie?api_key=2696829a81b1b5827d515ff121700838&query=batman&page=1
+
+        //construct the URL components
     if var urlComponents = URLComponents(string: "https://api.themoviedb.org/3/search/movie") {
       urlComponents.query = "api_key=2696829a81b1b5827d515ff121700838&query=\(searchTerm)&page=\(page)"
       
+        //combine the URL components to construct the url
       guard let url = urlComponents.url else {
         return
       }
@@ -33,13 +50,16 @@ class MovieManager: UITableViewController {
           self?.dataTask = nil
         }
         
+        //If you get an error, return it
         if let error = error {
           self?.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
         } else if
+            //save the data
           let data = data,
+            //make sure that the server returned a positive response
           let response = response as? HTTPURLResponse,
           response.statusCode == 200 {
-
+            //call updateSearchResults to parse & save the data
             self?.updateSearchResults(data)
           
           DispatchQueue.main.async {
@@ -52,35 +72,54 @@ class MovieManager: UITableViewController {
     }
   }
   
+    /**
+     * Updates the searchResults to be used elsewhere in the program
+     *
+     * @Param data the JSON data to parse into objects
+     */
   private func updateSearchResults(_ data: Data) {
-    //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    //let managedContext = AppDelegate.persistentContainer.viewContext
 
+    //create a JSO dictionary to save into
     var response: JSONDictionary?
+    //delete the previous results
     moviesResult.removeAll()
     do {
+        //attempt to cast the response as JSON
       response = try JSONSerialization.jsonObject(with: data, options: []) as? JSONDictionary
     } catch let parseError as NSError {
       errorMessage += "JSONSerialization error: \(parseError.localizedDescription)\n"
       return
     }
+    //convert the response into an array
     guard let array = response!["results"] as? [Any] else {
       errorMessage += "Dictionary does not contain results key\n"
       return
     }
     
     var index = 0
+    
+    //Create a formatter to parse the date
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
 
+    //for each movie
     for movieDictionary in array {
-        
+        //if the current dictionary is JSON
         if let MovieDictionary = movieDictionary as? JSONDictionary {
+            //create a new movie & populate it with the data
             let newMovie = Movie(context: context)
             newMovie.title = MovieDictionary["title"] as? String
-            newMovie.releaseDate = MovieDictionary["release_date"] as? Date
+            
+            //Use the date formatter to parse the release date
+            if let date = formatter.date(from: MovieDictionary["release_date"] as! String) {
+                newMovie.releaseDate = date
+            }
+            
             let posterURL = MovieDictionary["poster_path"] as! String
             newMovie.poster = MovieManager.BASE_IMAGE_URL + posterURL
             newMovie.overview = MovieDictionary["overview"] as? String
             
+            //add the new movie to the result
             moviesResult.append(newMovie)
             index += 1
         } else {
@@ -88,10 +127,4 @@ class MovieManager: UITableViewController {
         }
     }
   }
-}
-
-extension Data {
-    func toString() -> String? {
-        return String(data: self, encoding: .utf8)
-    }
 }
--- MovieApp/MovieApp/Controllers/SearchViewController.swift
+++ MovieApp/MovieApp/Controllers/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MovieApp
 //
 //  Created by Brandan Kalsow on 8/18/20.
@@ -14,11 +14,12 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var MovieTableView: UITableView!
     @IBOutlet weak var RecentSearchesTable: UITableView!
     
+    //save the searched movies & recent searches
     var movieArray = [Movie]()
     var last10Searches = [String]()
     
+    //make a new movieManager to query the site
     let movieManager = MovieManager()
-    //private static let BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w92"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,9 @@ class SearchViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    /**
+     * Toggles between the recent searches table and the movie table
+     */
     func toggleView()
     {
         self.RecentSearchesTable.isHidden = !RecentSearchesTable.isHidden
@@ -34,7 +38,7 @@ class SearchViewController: UIViewController {
     }
 }
 
-//MARK: Tableview Data Source Methods
+//MARK: Tableview Data Source Methods -
 extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
     
     //Get number of rows
@@ -42,11 +46,11 @@ extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
         return movieArray.count
     }
     
+    //Constructs each cell in the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTableViewCell
         cell.title.text = movieArray[indexPath.row].title
-        print(movieArray[indexPath.row].releaseDate as Any)
-        cell.releaseDate.text = movieArray[indexPath.row].releaseDate?.toString(dateFormat: "MM/dd/yyyy")
+        cell.releaseDate.text = movieArray[indexPath.row].releaseDate?.toString(dateFormat: "MMMM dd, yyyy")
         cell.overview.text = movieArray[indexPath.row].overview
         let url = self.movieArray[indexPath.row].poster
         cell.poster.load(url: URL(string: url!)!)
@@ -55,13 +59,17 @@ extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
     }
 }
 
+
+
 //MARK: Search Bar Delegate Methods
 extension SearchViewController: UISearchBarDelegate {
     
+    //When the user starts editing what's in the search bar
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         toggleView()
     }
     
+    //when the user clicks the cancel button
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         toggleView()
         
@@ -78,26 +86,32 @@ extension SearchViewController: UISearchBarDelegate {
         }
     } */
     
+    //When the user clicks the "Search" button
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
+        //toggle back to the movie table
         toggleView()
         
+        //verify that the search text isn't empty
         if searchBar.text!.count > 0 {
-            if(last10Searches.contains(searchBar.text!))
-            {
-                let search = searchBar.text!
-                last10Searches.remove(at: last10Searches.firstIndex(of: search)!)
-                last10Searches.append(searchBar.text!)
-            }
-            if last10Searches.count < 10 {
-                last10Searches.append(searchBar.text!)
-            } else {
-                last10Searches.removeFirst()
-                last10Searches.append(searchBar.text!)
-            }
-            print(last10Searches)
             
-            movieManager.getSearchResults(searchTerm: searchBar.text!) { [weak self] results, errorMessage in
+            let search = searchBar.text!
+            
+            //if the user searched this before, move it to the front
+            if(last10Searches.contains(search))
+            {
+                last10Searches.remove(at: last10Searches.firstIndex(of: search)!)
+                last10Searches.append(search)
+            } //else, if they haven't searched 10 times
+            else if last10Searches.count < 10 {
+                last10Searches.append(search)
+            } else { //otherwise, they've searched 10+ times and we need to cycle the first search out
+                last10Searches.removeFirst()
+                last10Searches.append(search)
+            }
+            
+            //get the search results for the query
+            movieManager.getSearchResults(searchTerm: search) { [weak self] results, errorMessage in
                      UIApplication.shared.isNetworkActivityIndicatorVisible = false
                
                      if let results = results {
@@ -105,7 +119,7 @@ extension SearchViewController: UISearchBarDelegate {
                        self?.MovieTableView.reloadData()
                        self?.MovieTableView.setContentOffset(CGPoint.zero, animated: false)
                      }
-                     
+                     //if there's an error message, print it to the console
                      if !errorMessage.isEmpty {
                        print("Search error: " + errorMessage)
                      }
@@ -113,7 +127,8 @@ extension SearchViewController: UISearchBarDelegate {
             DispatchQueue.main.async {
                 searchBar.resignFirstResponder()
             }
-            movieArray = movieManager.moviesResult
+            
+            //if search is empty, alert the user
         } else {
             let alert = UIAlertController(title: "Please enter a search term", message: "", preferredStyle: .alert)
             present(alert, animated: true, completion: nil)
